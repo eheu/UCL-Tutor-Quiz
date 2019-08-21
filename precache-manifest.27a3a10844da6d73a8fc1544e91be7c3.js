0a5c9f197139a6862c47be9d849ca96b5a53899a
Emil <eheu@libertymail.net>
2019-08-21 21:10:01 +0200

    Updates

---
+++ precache-manifest.27a3a10844da6d73a8fc1544e91be7c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1658f597b849a5a5becc3e150ed3ae31",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "856c768fd3cd036885e1",
+    "url": "/quiz/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e3b086d09dc3486ff70",
+    "url": "/quiz/static/css/main.3ab93fd6.chunk.css"
+  },
+  {
+    "revision": "856c768fd3cd036885e1",
+    "url": "/quiz/static/js/2.433d6be7.chunk.js"
+  },
+  {
+    "revision": "3e3b086d09dc3486ff70",
+    "url": "/quiz/static/js/main.cea90804.chunk.js"
+  },
+  {
+    "revision": "a01f2cced9f438b9243b",
+    "url": "/quiz/static/js/runtime~main.6a61ae30.js"
+  }
+]);
