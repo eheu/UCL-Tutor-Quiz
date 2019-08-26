3d8c19c727db05f0c0ab89574ffa7101b1220f2d
eheu <eheu@libertymail.net>
2019-08-26 12:49:54 +0200

    Updates

--- precache-manifest.a0df51b6c6d6028d318bbea2dfb5cab8.js
+++ precache-manifest.a0df51b6c6d6028d318bbea2dfb5cab8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "feaf7a8e81f201bf295400d43b4d73c5",
+    "revision": "5c2f1cdabdfe81af790fb202ff81af46",
     "url": "/index.html"
   },
   {
-    "revision": "aa96740044b46d46eaf3",
+    "revision": "5cbb806651e21fa192a7",
     "url": "/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "c61eeab2f82ed7adf1e4",
+    "revision": "fafc05a3f6864ce3859b",
     "url": "/static/css/main.b1f93573.chunk.css"
   },
   {
-    "revision": "aa96740044b46d46eaf3",
-    "url": "/static/js/2.12cba5f0.chunk.js"
+    "revision": "5cbb806651e21fa192a7",
+    "url": "/static/js/2.945075aa.chunk.js"
   },
   {
-    "revision": "c61eeab2f82ed7adf1e4",
-    "url": "/static/js/main.85d0d12f.chunk.js"
+    "revision": "fafc05a3f6864ce3859b",
+    "url": "/static/js/main.ec6b41c0.chunk.js"
   },
   {
     "revision": "fef67c183d7f5c04043b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d77121253eb7aa6aa99f06b320b6246b.js"
+  "/precache-manifest.a0df51b6c6d6028d318bbea2dfb5cab8.js"
 );
 
 self.addEventListener('message', (event) => {
